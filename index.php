eb8d4d92a904ab2230b84685b92d19b579bcf5aa
Jacopo Felluga <jacopo.felluga@gmail.com>
2026-05-17 22:39:32 +0200

    added lessons

--- index.php
+++ index.php
@@ -45,6 +45,39 @@
             }
         ?>
         </table>
+        <!-- lezioni -->
+        <hr />    
+        <h3>Lezioni</h3>
+        <div class="row">
+            <div class="col-6">
+                <button class="btn btn-primary mb-3">Visualizza Tutti</button>
+            </div>
+            <div class="col-6">
+                <button class="btn btn-primary mb-3">Aggiungi Lezione</button>
+            </div>
+        </div>
+        <table class="table table-striped">
+            <tr>
+                <th>Nome Corso</th>
+                <th>Data</th>
+                <th>Ora Inizio</th>
+                <th>Ora Fine</th>
+                <th>Sala</th>
+            </tr>
+        <?php
+            // $sql = "SELECT * FROM Lezioni WHERE Data >= CURDATE() ORDER BY Data  LIMIT 10;";
+            $sql = "SELECT *, Corsi.Nome AS NomeCorso, Sale.Nome AS NomeSala FROM Lezioni INNER JOIN Corsi ON Lezioni.IdCorso = Corsi.Id INNER JOIN Sale ON Corsi.IdSala = Sale.Id WHERE Data >= '2026-06-29' ORDER BY Data LIMIT 10;";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+            if($resultCheck > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $datePieces = explode("-", $row['Data']);
+                    $convertedDate = $datePieces[2]."/".$datePieces[1]."/".$datePieces[0];
+                    echo "<tr><td>" . $row['NomeCorso'] . "</td><td>" . $convertedDate . "</td><td>" . $row['OraInizio'] . "</td><td>" . $row['OraFine'] . "</td><td>" . $row['NomeSala'] . "</td></tr>";
+                }
+            }
+        ?>
+        </table>
         
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
